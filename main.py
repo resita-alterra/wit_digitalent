25be773c56e71a04e985d3de89dd50d1160106e7
Resita Sri Wahyuni <resita@alterra.id>
2021-08-11 10:34:47 +0800

    add image

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ CHILD_MEASURE = 48
 #end1
 #2 loop
 # main loop of game Infinite loop 
+# child_rect = pygame.Rect(120,464,CHILD_MEASURE,CHILD_MEASURE)
+#3 let's add the bird
+child_image = pygame.image.load('asset/sprites_splitted/child.png').convert_alpha() 
+child_image = pygame.transform.scale(child_image, (CHILD_MEASURE, CHILD_MEASURE))
+# bg_surface = pygame.image.load('assets/background-day.png').convert()
+#rect for the position
+child_rect = child_image.get_rect(topleft = (120,464))
 while True:
     clock.tick(FPS) # ensure the event only at FPS setting 
     for event in pygame.event.get(): # get list of event
@@ -20,5 +27,5 @@ while True:
     
     # Fill the background with white
     SCREEN.fill((255, 255, 255))
-    pygame.draw.rect(SCREEN, (0, 0, 255), pygame.Rect(120,464,CHILD_MEASURE,CHILD_MEASURE))
+    SCREEN.blit(child_image,child_rect)
     pygame.display.update() # update render
